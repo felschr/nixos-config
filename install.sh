36b5207ebdfd433be1f4ec44b4ee685bb2a65d2c
Felix Tenley <dev@felschr.com>
2019-10-18 20:08:05 +0200

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 
 if [ $EUID != 0 ]; then
     sudo "$0" "$@"
     exit $?
 fi
 
-CONFIG=$0
+CONFIG=$1
 
 if [ -z "$CONFIG" ]
 then
   echo "path to config to use as configuration.nix needs to be passed as first argument"
+else
+  echo "using configuration: '$CONFIG'"
 fi
 
 ln -s $CONFIG configuration.nix
@@ -17,7 +19,7 @@ ln -s $CONFIG configuration.nix
 nixos-generate-config --root /mnt
 
 # add nixos-unstable and home-manager channels
-nix-channel --add http://nixos.org/channels/nixos-unstable nixos-unstable
+nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
 nix-channel --add https://github.com/rycee/home-manager/archive/release-19.03.tar.gz home-manager
 nix-channel --update
 
