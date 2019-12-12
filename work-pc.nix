8622740aa8c3d953498604a14591a3b306b096dc
Felix Schröter <felschr@gmail.com>
2019-12-12 15:47:59 +0100

    enable adb

--- work-pc.nix
+++ work-pc.nix
@@ -26,6 +26,8 @@
   hardware.cpu.intel.updateMicrocode = true;
   services.tlp.enable = true;
 
+  programs.adb.enable = true;
+
   users.users.felschr = {
     isNormalUser = true;
     extraGroups = [ "wheel" "audio" "docker" "disk" "vboxusers" ];
