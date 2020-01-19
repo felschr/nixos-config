0344e3f622890361731d494fcad3573a657c63dd
Felix Tenley <dev@felschr.com>
2020-01-19 10:46:39 +0100

    feat: add ntfs support

--- common/base-hardware.nix
+++ common/base-hardware.nix
@@ -7,6 +7,7 @@
       allowDiscards = true;
     };
   };
+  boot.supportedFilesystems = [ "ntfs" ];
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
