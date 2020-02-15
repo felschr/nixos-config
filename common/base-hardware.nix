814a1ead4e0726eeca6cc66adf7ca010865a98d2
Felix Tenley <dev@felschr.com>
2020-02-15 10:46:07 +0100

    feat: add smartd

--- common/base-hardware.nix
+++ common/base-hardware.nix
@@ -12,6 +12,9 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  services.smartd.enable = true;
+  services.smartd.notifications.x11.enable = true;
+
   # Bluetooth
   hardware.bluetooth.enable = true;
   hardware.bluetooth.config = {
