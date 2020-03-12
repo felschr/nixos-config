55b9993e5711ed8bf44a4a0546cadd9ed6e936ef
Felix Tenley <dev@felschr.com>
2020-03-12 15:41:24 +0100

    feat: add .editorconfig

---
+++ home/common/editors.nix
@@ -0,0 +1,33 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./neovim.nix
+    ./emacs.nix
+    ./vscode.nix
+  ];
+
+  home.packages = with pkgs; [
+    fzf
+    ripgrep
+  ];
+
+  home.sessionVariables = with pkgs; {
+    EDITOR = "nvim";
+    FZF_DEFAULT_COMMAND = "${ripgrep}/bin/rg --files --hidden";
+  };
+
+  home.file.".editorconfig".text = ''
+    [*]
+    charset = utf-8
+    indent_style = space
+    indent_size = 2
+    end_of_line = lf
+    insert_final_newline = true
+    trim_trailing_whitespace = true
+
+    [*.md]
+    insert_final_newline = false
+    trim_trailing_whitespace = false
+  '';
+}
