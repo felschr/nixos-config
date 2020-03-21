8a059c403750b08ddfa0a2863d27547a2e944e08
Felix Tenley <dev@felschr.com>
2020-03-21 17:06:28 +0100

    feat(signal): create wrapper to always set --use-tray-icon

--- home/signal.nix
+++ home/signal.nix
@@ -1,5 +1,19 @@
 { config, pkgs, ... }:
 
+let
+  signal-desktop = pkgs.runCommand "signal-desktop" {
+    buildInputs = [ pkgs.makeWrapper ];
+  } ''
+    mkdir $out
+    ln -s ${pkgs.signal-desktop}/* $out
+    rm $out/bin
+    mkdir $out/bin
+    ln -s ${pkgs.signal-desktop}/bin/* $out/bin
+    rm $out/bin/signal-desktop
+    makeWrapper ${pkgs.signal-desktop}/bin/signal-desktop $out/bin/signal-desktop \
+      --add-flags "--use-tray-icon"
+  '';
+in
 {
-  home.packages = [ pkgs.signal-desktop ];
+  home.packages = [ signal-desktop ];
 }
