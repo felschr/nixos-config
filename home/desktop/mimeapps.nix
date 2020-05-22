b80031723a3967a1ec0dfc1d8a2040bfdc553f7f
Felix Tenley <dev@felschr.com>
2020-05-22 18:02:31 +0200

    feat: update mimeapps

--- home/desktop/mimeapps.nix
+++ home/desktop/mimeapps.nix
@@ -1,23 +1,24 @@
-{ config, pkgs, ... }:  
+{ config, pkgs, ... }:
 
 {
   xdg.mimeApps.enable = true;
   xdg.mimeApps.defaultApplications = {
-    "image/gif" = [ "eog.desktop" ];
-    "image/jpeg" = [ "eog.desktop" ];
-    "image/jpg" = [ "eog.desktop" ];
-    "image/png" = [ "eog.desktop" ];
-    "image/webp" = [ "eog.desktop" ];
+    "application/pdf" = [ "org.gnome.Evince.desktop" ];
+    "image/gif" = [ "org.gnome.eog.desktop" ];
+    "image/jpeg" = [ "org.gnome.eog.desktop" ];
+    "image/jpg" = [ "org.gnome.eog.desktop" ];
+    "image/png" = [ "org.gnome.eog.desktop" ];
+    "image/webp" = [ "org.gnome.eog.desktop" ];
     "inode/directory" = [ "nautilus.desktop" "org.gnome.Nautilus.desktop" ];
-    "text/calendar" = [ "chromium-browser.desktop" ];
-    "text/html" = [ "chromium-browser.desktop" ];
+    "text/calendar" = [ "brave-browser.desktop" ];
+    "text/html" = [ "brave-browser.desktop" ];
     "text/plain" = [ "org.gnome.gedit.desktop" ];
-    "x-scheme-handler/about" = [ "chromium-browser.desktop" ];
-    "x-scheme-handler/http" = [ "chromium-browser.desktop" ];
-    "x-scheme-handler/https" = [ "chromium-browser.desktop" ];
-    "x-scheme-handler/mailto" = [ "chromium-browser.desktop" ];
+    "x-scheme-handler/about" = [ "brave-browser.desktop" ];
+    "x-scheme-handler/http" = [ "brave-browser.desktop" ];
+    "x-scheme-handler/https" = [ "brave-browser.desktop" ];
+    "x-scheme-handler/mailto" = [ "brave-browser.desktop" ];
     "x-scheme-handler/sgnl" = [ "signal-desktop.desktop" ];
-    "x-scheme-handler/unknown" = [ "chromium-browser.desktop" ];
-    "x-scheme-handler/webcal" = [ "chromium-browser.desktop" ];
+    "x-scheme-handler/unknown" = [ "brave-browser.desktop" ];
+    "x-scheme-handler/webcal" = [ "brave-browser.desktop" ];
   };
 }
