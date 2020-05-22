9c8c2114ce27c8a484d57e6a3998af73b22c7e46
Felix Tenley <dev@felschr.com>
2020-05-22 17:59:54 +0200

    feat: add brave

---
+++ home/browser.nix
@@ -0,0 +1,15 @@
+{ config, pkgs, ... }:
+
+{
+  programs.chromium = {
+    enable = true;
+    package = pkgs.chromium.override {
+      enableVaapi = true; # NVIDIA also requires vdpau backend
+      commandLineArgs = "--force-dark-mode";
+    };
+  };
+
+  home.packages = with pkgs; [
+    brave
+  ];
+}
