b27939a07cce8a0382bb32c22a953d969c13cda9
Felix Tenley <dev@felschr.com>
2020-05-29 17:12:17 +0200

    chore(omnisharp): remove override for omnisharp-roslyn

--- home/editors/omnisharp.nix
+++ home/editors/omnisharp.nix
@@ -1,16 +1,7 @@
 { config, pkgs, ... }:
 
-let
-  omnisharp-roslyn = pkgs.omnisharp-roslyn.overrideAttrs (oldAttrs: rec {
-      version = "1.35.1";
-      src = pkgs.fetchurl {
-        url = "https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v${version}/omnisharp-mono.tar.gz";
-        sha256 = "0gx87qc9r3lhqn6q95y74z67sjcxnazkkdi9zswmaqyvjn8x7vf4";
-      };
-  });
-in
 {
-  home.packages = [
+  home.packages = with pkgs; [
     omnisharp-roslyn
   ];
 
