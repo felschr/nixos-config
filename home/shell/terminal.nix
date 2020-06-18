c56561aa350118f3a452c70e6d6eb203acacb8da
Felix Tenley <dev@felschr.com>
2020-06-18 16:25:53 +0200

    feat: add alacritty

--- home/shell/terminal.nix
+++ home/shell/terminal.nix
@@ -1,6 +1,22 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
+with pkgs; with lib;
+let
+  yamlToJSON = path: runCommand "yaml.json" { nativeBuildInputs = [ pkgs.ruby ]; } ''
+    ruby -rjson -ryaml -e "puts YAML.load(ARGF).to_json" < ${path} > $out
+  '';
+in
 {
+  # doesn't support font ligatures yet
+  # emoji support needs to be setup
+  # use with tabbed for tab support: tabbed -c alacritty --embed
+  # TODO create PR for adding programs.alacritty.package
+  programs.alacritty = {
+    enable = true;
+    settings = recursiveUpdate {
+    } (trivial.importJSON (yamlToJSON ./alacritty-gruvbox-dark.yml));
+  };
+
   programs.kitty = {
     enable = true;
     font = {
