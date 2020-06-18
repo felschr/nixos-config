b899f9caef23e99e3d48ce636aaea13e2be32e86
Felix Tenley <dev@felschr.com>
2020-06-18 16:18:57 +0200

    feat: fix pop-gtk-theme

--- home/desktop/gtk.nix
+++ home/desktop/gtk.nix
@@ -1,9 +1,17 @@
 { config, pkgs, ... }:
 
+let
+  pop-gtk-theme = pkgs.pop-gtk-theme.overrideAttrs (oldAttrs: rec {
+    postInstall = ''
+      # fix dark gnome-shell theme
+      ln -s $out/share/gnome-shell/theme/Pop-dark $out/share/themes/Pop-dark/gnome-shell
+    '';
+  });
+in
 {
   gtk.enable = true;
   gtk.theme.name = "Pop-dark";
-  gtk.theme.package = pkgs.pop-gtk-theme;
+  gtk.theme.package = pop-gtk-theme;
   gtk.gtk3.extraConfig = {
     gtk-application-prefer-dark-theme = true;
   };
