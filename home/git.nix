844fe5868c20e8ddcda40cb52f5514ec0584f5ea
Felix Tenley <dev@felschr.com>
2020-08-15 00:10:53 +0200

    feat: extend git config

--- home/git.nix
+++ home/git.nix
@@ -58,8 +58,22 @@ in
         signByDefault = true;
       };
       extraConfig = {
-        pull = { rebase = true; };
-        rebase = { autoStash = true; };
+        init = {
+          defaultBranch = "main";
+        };
+        pull = {
+          rebase = true;
+        };
+        rebase = {
+          autoStash = true;
+          autoSquash = true;
+          abbreviateCommands = true;
+          missingCommitsCheck = "warn";
+        };
+      };
+      aliases = {
+        mr = "!sh -c 'git fetch $1 merge-requests/$2/head:mr-$1-$2 && git checkout mr-$1-$2' -";
+        pr = "!sh -c 'git fetch $1 refs/pull/$2/head:pr/$1 && git checkout pr/$2'";
       };
       includes = flatten (mapAttrsToList (name: profile: map (dir: {
         condition = "gitdir:${dir}";
