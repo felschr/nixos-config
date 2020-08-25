f0f5a05bd099301691c8e78e0d0a52c14f8f6235
Felix Tenley <dev@felschr.com>
2020-08-25 21:53:05 +0200

    feat: update git profile dirs

--- home/git.nix
+++ home/git.nix
@@ -12,13 +12,13 @@
         name       = "Felix Tenley";
         email      = "dev@felschr.com";
         signingKey = "6AB3 7A28 5420 9A41 82D9  0068 910A CB9F 6BD2 6F58";
-        dirs       = [ "/etc/nixos/" ];
+        dirs       = [ "~/dev/private/" "/etc/nixos" ];
       };
       work = {
         name       = "Felix Schröter";
         email      = "fs@upsquared.com";
         signingKey = "F28B FB74 4421 7580 5A49  2930 BE85 F0D9 987F A014";
-        dirs       = [ "~/dev/" ];
+        dirs       = [ "~/dev/work/" ];
       };
     };
     defaultProfile = "private";
