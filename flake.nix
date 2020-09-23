56fd0a749ecf2519265e4ddad8f9f8850fcd3e8d
Felix Tenley <dev@felschr.com>
2020-09-23 13:41:46 +0200

    refactor(flake): simplify inputs.home-manager

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  inputs.home-manager = { url = "github:nix-community/home-manager/master"; };
+  inputs.home-manager.url = "github:nix-community/home-manager/master";
 
   inputs.nur.url = "github:nix-community/NUR/master";
 
