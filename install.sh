fa6981e62069613c991bcab15ade9450b1279072
Felix Tenley <dev@felschr.com>
2020-09-23 09:40:08 +0200

    fix: update home-manager channel

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ nixos-generate-config --root /mnt
 
 # add nixos-unstable and home-manager channels
 nix-channel --add https://nixos.org/channels/nixos-unstable nixos
-nix-channel --add https://github.com/rycee/home-manager/archive/master.tar.gz home-manager
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 
 nixos-install
