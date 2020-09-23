edd41720018d45599107be02cb5e06a67ac55811
Felix Tenley <dev@felschr.com>
2020-09-23 09:37:39 +0200

    feat(signal-desktop): simplify wrapper

--- home/signal.nix
+++ home/signal.nix
@@ -1,17 +1,15 @@
 { config, pkgs, ... }:
 
+with pkgs;
 let
   # TODO this doesn't affect the desktop file
   # e.g. when starting via GNOME the flag is not set
-  signal-desktop = pkgs.runCommand "signal-desktop" {
-    buildInputs = [ pkgs.makeWrapper ];
+  signal-desktop = runCommand "signal-desktop" {
+    buildInputs = [ makeWrapper ];
   } ''
     mkdir $out
     ln -s ${pkgs.signal-desktop}/* $out
     rm $out/bin
-    mkdir $out/bin
-    ln -s ${pkgs.signal-desktop}/bin/* $out/bin
-    rm $out/bin/signal-desktop
     makeWrapper ${pkgs.signal-desktop}/bin/signal-desktop $out/bin/signal-desktop \
       --add-flags "--use-tray-icon"
   '';
