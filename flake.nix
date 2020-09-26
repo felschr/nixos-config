10a370ceac50187eb071f4173b0fa62617d27972
Felix Tenley <dev@felschr.com>
2020-09-26 23:55:15 +0200

    feat(flake): reference nixpkgs input in other inputs

--- flake.nix
+++ flake.nix
@@ -3,13 +3,19 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  inputs.home-manager.url = "github:nix-community/home-manager/master";
+  inputs.home-manager = {
+    url = "github:nix-community/home-manager/master";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
 
   inputs.nur.url = "github:nix-community/NUR/master";
 
-  inputs.pre-commit-hooks.url =
-    # "github:Myhlamaeus/pre-commit-hooks.nix/feat/flake";
-    "github:Myhlamaeus/pre-commit-hooks.nix/8d48a4cd434a6a6cc8f2603b50d2c0b2981a7c55";
+  inputs.pre-commit-hooks = {
+    url =
+      # "github:Myhlamaeus/pre-commit-hooks.nix/feat/flake";
+      "github:Myhlamaeus/pre-commit-hooks.nix/8d48a4cd434a6a6cc8f2603b50d2c0b2981a7c55";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
 
   outputs = { self, nixpkgs, flake-utils, home-manager, nur, pre-commit-hooks }:
     let
