2bfc37f8d8fff1c9384c5e0cd07b746d42334874
Felix Tenley <dev@felschr.com>
2020-10-07 02:03:02 +0200

    feat(jellyfin): add jellyfin user to video group

--- services/jellyfin.nix
+++ services/jellyfin.nix
@@ -4,4 +4,7 @@
   services.jellyfin.enable = true;
   services.jellyfin.user = "felschr";
   networking.firewall.allowedTCPPorts = [ 8920 8096 1900 7359 ];
+
+  # for hardware acceleration
+  users.users.jellyfin.extraGroups = [ "video" ];
 }
