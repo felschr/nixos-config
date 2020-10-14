7b04776cc3534fc4f44ec972535d06bfc0ebaa47
Felix Tenley <dev@felschr.com>
2020-10-14 18:41:27 +0200

    feat(keybase): remove keybase-gui from autostart

--- home/keybase.nix
+++ home/keybase.nix
@@ -5,9 +5,4 @@
   services.kbfs.enable = true;
 
   home.packages = [ pkgs.keybase-gui ];
-
-  xdg.configFile."autostart/keybase.desktop".text = with pkgs;
-    builtins.replaceStrings [ "${keybase-gui}" ]
-    [ "env KEYBASE_AUTOSTART=1 ${keybase-gui}" ]
-    (builtins.readFile "${keybase-gui}/share/applications/keybase.desktop");
 }
