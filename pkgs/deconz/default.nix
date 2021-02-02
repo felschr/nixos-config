b8066bf4f933b0c94ffab592b6f0bf5697687b3c
Felix Tenley <dev@felschr.com>
2021-02-02 18:31:25 +0100

    feat: switch from stdenv.lib to lib

--- pkgs/deconz/default.nix
+++ pkgs/deconz/default.nix
@@ -1,14 +1,13 @@
-{ stdenv, fetchurl, mkDerivation, dpkg, autoPatchelfHook
-, qtserialport, qtwebsockets
-, libredirect, makeWrapper, gzip, gnutar
-}:
+{ lib, fetchurl, mkDerivation, dpkg, autoPatchelfHook, qtserialport
+, qtwebsockets, libredirect, makeWrapper, gzip, gnutar }:
 
 mkDerivation rec {
   name = "deconz-${version}";
   version = "2.05.82";
 
   src = fetchurl {
-    url = "https://deconz.dresden-elektronik.de/raspbian/alpha/deconz_${version}-debian-stretch-beta_arm64.deb";
+    url =
+      "https://deconz.dresden-elektronik.de/raspbian/alpha/deconz_${version}-debian-stretch-beta_arm64.deb";
     sha256 = "cCH7XhRXCHKm5AVsM19TyHwAjhbTv4qyDx2GamuDWQw=";
   };
 
@@ -27,14 +26,16 @@ mkDerivation rec {
     wrapProgram "$out/bin/deCONZ" \
         --set LD_PRELOAD "${libredirect}/lib/libredirect.so" \
         --set NIX_REDIRECTS "/usr/share=$out/share:/usr/bin=$out/bin" \
-        --prefix PATH : "${stdenv.lib.makeBinPath [ gzip gnutar ]}"
+        --prefix PATH : "${lib.makeBinPath [ gzip gnutar ]}"
   '';
 
-  meta = with stdenv.lib; {
-    description = "Manage ZigBee network with ConBee, ConBee II or RaspBee hardware";
+  meta = with lib; {
+    description =
+      "Manage ZigBee network with ConBee, ConBee II or RaspBee hardware";
     # 2019-08-19: The homepage links to old software that doesn't even work --
     # it fails to detect ConBee2.
-    homepage = "https://www.dresden-elektronik.de/funktechnik/products/software/pc-software/deconz/?L=1";
+    homepage =
+      "https://www.dresden-elektronik.de/funktechnik/products/software/pc-software/deconz/?L=1";
     license = licenses.unfree;
     platforms = with platforms; linux;
     maintainers = with maintainers; [ felschr ];
