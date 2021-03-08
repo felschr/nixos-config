2e82835d6f3b96438fd8ee126098c4c2bc71ffef
Felix Tenley <dev@felschr.com>
2021-03-08 19:44:54 +0100

    feat(neovim): expand nvim-treesitter config

--- home/editors/neovim/treesitter.lua
+++ home/editors/neovim/treesitter.lua
@@ -7,10 +7,18 @@ require'nvim-treesitter.configs'.setup {
   },
   indent = {
     enable = true,
+    -- TODO seems to be broken atm
+    disable = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
   },
   refactor = {
     highlight_definitions = { enable = true },
     highlight_current_scope = { enable = true },
+    smart_rename = {
+      enable = true,
+      keymaps = {
+        smart_rename = "grr",
+      },
+    },
     navigation = {
       enable = true,
       keymaps = {
@@ -41,6 +49,34 @@ require'nvim-treesitter.configs'.setup {
         ["ic"] = "@class.inner",
         ["aa"] = "@parameter.outer",
         ["ia"] = "@parameter.inner",
+      },
+    },
+    swap = {
+      enable = true,
+      swap_next = {
+        ["<C-l>"] = "@parameter.inner",
+      },
+      swap_previous = {
+        ["<C-h>"] = "@parameter.inner",
+      },
+    },
+    move = {
+      enable = true,
+      goto_next_start = {
+        ["]m"] = "@function.outer",
+        ["]]"] = "@class.outer",
+      },
+      goto_next_end = {
+        ["]M"] = "@function.outer",
+        ["]["] = "@class.outer",
+      },
+      goto_previous_start = {
+        ["[m"] = "@function.outer",
+        ["[["] = "@class.outer",
+      },
+      goto_previous_end = {
+        ["[M"] = "@function.outer",
+        ["[]"] = "@class.outer",
       },
     },
   },
