06da29dac31943c86b00752df95b968fd7bdfc68
Felix Tenley <dev@felschr.com>
2021-04-16 15:19:48 +0200

    feat(etebase-server): update configuration

--- services/etebase.nix
+++ services/etebase.nix
@@ -1,13 +1,13 @@
 { config, pkgs, ... }:
 
-let
-  etebaseHost = "etebase.felschr.com";
-in
-{
+let etebaseHost = "etebase.felschr.com";
+in {
   services.etebase-server.enable = true;
-  services.etebase-server.host = etebaseHost;
   services.etebase-server.openFirewall = true;
-  services.etebase-server.secretFile = "/etc/nixos/secrets/etebase-server";
+  services.etebase-server.settings = {
+    global = { secret_file = "/etc/nixos/secrets/etebase-server"; };
+    allowed_hosts = { allowed_host1 = etebaseHost; };
+  };
 
   services.nginx = {
     virtualHosts."${etebaseHost}" = {
