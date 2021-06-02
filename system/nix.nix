31fe83a0567cf0394e83867cd90ce93cee3d3aae
Felix Tenley <dev@felschr.com>
2021-06-02 19:17:20 +0200

    feat(nix): add IOHK cache for haskell.nix

--- system/nix.nix
+++ system/nix.nix
@@ -14,7 +14,10 @@
     options = "--delete-older-than 30d";
   };
 
-  nix.binaryCaches = [ "https://nixcache.reflex-frp.org" ];
-  nix.binaryCachePublicKeys =
-    [ "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI=" ];
+  nix.binaryCaches =
+    [ "https://hydra.iohk.io" "https://nixcache.reflex-frp.org" ];
+  nix.binaryCachePublicKeys = [
+    "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI="
+  ];
 }
