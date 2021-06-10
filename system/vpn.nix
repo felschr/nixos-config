713e5072987150cd3ab65d409955cb9f66b671c1
Felix Tenley <dev@felschr.com>
2021-06-10 12:14:30 +0200

    fix(vpn): add workaround for mullvad issues

--- system/vpn.nix
+++ system/vpn.nix
@@ -2,7 +2,9 @@
 
 {
   networking.wireguard.enable = true;
-  networking.iproute2.enable = true;
+
+  # TODO fix for https://github.com/NixOS/nixpkgs/issues/113589
+  networking.firewall.checkReversePath = "loose";
 
   services.mullvad-vpn.enable = true;
 }
