476d19ccf99b4864eb10784b9927bb9cdb333bc4
Felix Tenley <dev@felschr.com>
2021-08-04 21:34:13 +0200

    chore(flake): update inputs

--- services/syncthing/felix-nixos.nix
+++ services/syncthing/felix-nixos.nix
@@ -12,36 +12,32 @@ in {
     user = "felschr";
     configDir = "/home/felschr/.config/syncthing";
     dataDir = "/home/felschr/.local/share/syncthing";
-    declarative = {
-      cert = "/etc/nixos/secrets/syncthing/cert.pem";
-      key = "/etc/nixos/secrets/syncthing/key.pem";
-      devices = {
-        rpi4 = {
-          id =
-            "RBKVWQQ-TGYBMQK-P4AADKE-7LGPHL7-UY4FEZA-6N7HQ4R-UCPSZPV-LWFK4AP";
-        };
-        pixel3 = {
-          id =
-            "YM26ZEC-V5QQTOI-7U355KQ-WTHLL3X-H3YVJR5-4UPM5SS-YXWGVGA-EBWZEQP";
-        };
+    cert = "/etc/nixos/secrets/syncthing/cert.pem";
+    key = "/etc/nixos/secrets/syncthing/key.pem";
+    devices = {
+      rpi4 = {
+        id = "RBKVWQQ-TGYBMQK-P4AADKE-7LGPHL7-UY4FEZA-6N7HQ4R-UCPSZPV-LWFK4AP";
       };
-      folders = {
-        "Default" = {
-          id = "default";
-          path = "/home/felschr/sync/default";
-          devices = [ "rpi4" "pixel3" ];
-        };
-        "Backups" = {
-          id = "backups";
-          path = "/home/felschr/sync/backups";
-          devices = [ "rpi4" ];
-        };
-        "Media" = {
-          id = "media";
-          path = "/run/media/felschr/HDD/Media";
-          devices = [ "rpi4" ];
-          inherit versioning;
-        };
+      pixel3 = {
+        id = "YM26ZEC-V5QQTOI-7U355KQ-WTHLL3X-H3YVJR5-4UPM5SS-YXWGVGA-EBWZEQP";
+      };
+    };
+    folders = {
+      "Default" = {
+        id = "default";
+        path = "/home/felschr/sync/default";
+        devices = [ "rpi4" "pixel3" ];
+      };
+      "Backups" = {
+        id = "backups";
+        path = "/home/felschr/sync/backups";
+        devices = [ "rpi4" ];
+      };
+      "Media" = {
+        id = "media";
+        path = "/run/media/felschr/SSD/Media";
+        devices = [ "rpi4" ];
+        inherit versioning;
       };
     };
   };
--- services/syncthing/rpi4.nix
+++ services/syncthing/rpi4.nix
@@ -12,40 +12,37 @@ in {
     user = "felschr";
     configDir = "/home/felschr/.config/syncthing";
     dataDir = "/home/felschr/.local/share/syncthing";
-    declarative = {
-      cert = "/etc/nixos/secrets/syncthing/cert.pem";
-      key = "/etc/nixos/secrets/syncthing/key.pem";
-      devices = {
-        felix-nixos = {
-          id =
-            "MKVEPJK-HWGFVLN-SRHE4NR-ZADXKMF-AMYIKZ7-MGUJH3L-XH2FBOW-AMQRIAW";
-        };
-        pixel3 = {
-          id =
-            "YM26ZEC-V5QQTOI-7U355KQ-WTHLL3X-H3YVJR5-4UPM5SS-YXWGVGA-EBWZEQP";
-        };
+
+    cert = "/etc/nixos/secrets/syncthing/cert.pem";
+    key = "/etc/nixos/secrets/syncthing/key.pem";
+    devices = {
+      felix-nixos = {
+        id = "MKVEPJK-HWGFVLN-SRHE4NR-ZADXKMF-AMYIKZ7-MGUJH3L-XH2FBOW-AMQRIAW";
       };
-      # TODO switch to external storage
-      folders = {
-        "Default" = {
-          id = "default";
-          path = "/home/felschr/sync/default";
-          devices = [ "felix-nixos" "pixel3" ];
-          inherit versioning;
-        };
-        "Backups" = {
-          id = "backups";
-          path = "/home/felschr/sync/backups";
-          devices = [ "felix-nixos" ];
-          inherit versioning;
-        };
-        "Media" = {
-          id = "media";
-          path = "/media";
-          # path = "/media/inbox";
-          devices = [ "felix-nixos" ];
-          inherit versioning;
-        };
+      pixel3 = {
+        id = "YM26ZEC-V5QQTOI-7U355KQ-WTHLL3X-H3YVJR5-4UPM5SS-YXWGVGA-EBWZEQP";
+      };
+    };
+    # TODO switch to external storage
+    folders = {
+      "Default" = {
+        id = "default";
+        path = "/home/felschr/sync/default";
+        devices = [ "felix-nixos" "pixel3" ];
+        inherit versioning;
+      };
+      "Backups" = {
+        id = "backups";
+        path = "/home/felschr/sync/backups";
+        devices = [ "felix-nixos" ];
+        inherit versioning;
+      };
+      "Media" = {
+        id = "media";
+        path = "/media";
+        # path = "/media/inbox";
+        devices = [ "felix-nixos" ];
+        # inherit versioning;
       };
     };
   };
