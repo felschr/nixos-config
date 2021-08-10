642f7406f6955653824aa0181653aa4458f2ec6d
Felix Tenley <dev@felschr.com>
2021-08-10 15:56:45 +0200

    fix: remove duplicate nix option

--- system/nix.nix
+++ system/nix.nix
@@ -1,12 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  # for flakes support
-  nix.package = pkgs.nixUnstable;
-  nix.extraOptions = ''
-    experimental-features = nix-command flakes
-  '';
-
   nix.autoOptimiseStore = true;
   nix.gc = {
     automatic = true;
