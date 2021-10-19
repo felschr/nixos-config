ac677032b9a80a0b6519c763a2abcc289b3570bc
Felix Tenley <dev@felschr.com>
2021-10-19 11:26:22 +0200

    fix: fix pop-shell

--- pkgs/pop-shell/default.nix
+++ pkgs/pop-shell/default.nix
@@ -2,18 +2,26 @@
 
 stdenv.mkDerivation rec {
   pname = "pop-shell";
-  version = "2021-09-10";
+  version = "2021-10-18";
 
   src = fetchFromGitHub {
     owner = "pop-os";
     repo = "shell";
-    rev = "59ca9cede3c185c347bd2ae3d40882e020fb1fd0";
-    sha256 = "llwchrg/a8QmmD9eOt7IUZY2crYubFSDyvrTVwSz0pE=";
+    rev = "1de4139c5739ff79d7a3ddef7a9a77988a358782";
+    sha256 = "094siblqpsjj356s32dn3rqq0vd47xrmbklzzyx26nh4hxlvzkzb";
   };
 
   nativeBuildInputs = [ glib nodePackages.typescript gjs ];
 
   buildInputs = [ gjs ];
+
+  preBuildHook = ''
+    ./fix-gjs.patch
+    find . -name '*.ts' -exec sed -i -E 's|^#!/usr/bin/gjs|#!/usr/bin/env gjs|' \{\} \;
+    (fetchpatch {
+  '';
+
+  patches = [ ./gnome-41.patch ];
 
   # the gschema doesn't seem to be installed properly (see dconf)
   makeFlags = [
@@ -21,4 +29,13 @@ stdenv.mkDerivation rec {
     "PLUGIN_BASE=$(out)/share/pop-shell/launcher"
     "SCRIPTS_BASE=$(out)/share/pop-shell/scripts"
   ];
+
+  postInstall = ''
+    chmod +x $out/share/gnome-shell/extensions/pop-shell@system76.com/floating_exceptions/main.js
+    chmod +x $out/share/gnome-shell/extensions/pop-shell@system76.com/floating_exceptions/main.js
+    chmod +x $out/share/gnome-shell/extensions/pop-shell@system76.com/color_dialog/main.js
+    chmod +x $out/share/gnome-shell/extensions/pop-shell@system76.com/color_dialog/main.js
+    rm -rf $out/bin
+    rm -rf $out/bin
+  '';
 }
