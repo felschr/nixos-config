56a1a907e26003aad000ada93d44f78ef1af6312
Felix Tenley <dev@felschr.com>
2021-10-23 03:28:15 +0200

    feat(rpi4): update kodi config

--- services/kodi.nix
+++ services/kodi.nix
@@ -3,10 +3,22 @@
 {
   services.xserver.enable = true;
   services.xserver.desktopManager.kodi.enable = true;
+  services.xserver.desktopManager.kodi.package =
+    pkgs.kodi.withPackages (p: with p; [ jellyfin netflix youtube ]);
+
+  services.xserver.displayManager.lightdm.enable = true;
   services.xserver.displayManager.autoLogin.enable = true;
   services.xserver.displayManager.autoLogin.user = "kodi";
 
-  users.extraUsers.kodi.isNormalUser = true;
+  users.extraUsers.kodi = {
+    isNormalUser = true;
+    extraGroups = [ "video" "input" "audio" ];
+  };
+
+  # TODO doesn't seem to work
+  # services.cage.enable = true;
+  # services.cage.user = "kodi";
+  # services.cage.program = "${pkgs.kodi-wayland}/bin/kodi-standalone";
 
   networking.firewall = {
     allowedTCPPorts = [ 8080 ];
