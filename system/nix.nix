b37559ff70142f128dc4ca2e39ba03be26927d88
Felix Tenley <dev@felschr.com>
2021-10-29 20:26:43 +0200

    feat(nix): add shajra cache for hls with ghc 8.6.5 support

--- system/nix.nix
+++ system/nix.nix
@@ -8,10 +8,14 @@
     options = "--delete-older-than 30d";
   };
 
-  nix.binaryCaches =
-    [ "https://hydra.iohk.io" "https://nixcache.reflex-frp.org" ];
+  nix.binaryCaches = [
+    "https://hydra.iohk.io"
+    "https://nixcache.reflex-frp.org"
+    "https://shajra.cachix.org"
+  ];
   nix.binaryCachePublicKeys = [
     "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
     "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI="
+    "shajra.cachix.org-1:V0x7Wjgd/mHGk2KQwzXv8iydfIgLupbnZKLSQt5hh9o="
   ];
 }
