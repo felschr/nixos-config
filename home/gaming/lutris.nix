6a0f0a58e70ef19a98cf1cdc24e7d2825a3a4a56
Felix Tenley <dev@felschr.com>
2022-01-20 17:02:09 +0100

    feat(lutris): remove wine-runner-sc

--- home/gaming/lutris.nix
+++ home/gaming/lutris.nix
@@ -16,28 +16,5 @@
       };
       target = "lutris/runners/wine/${name}";
     };
-    wine-runner-sc = let
-      package = { }:
-        pkgs.stdenv.mkDerivation rec {
-          name = "wine-runner-sc";
-          version = "6.10";
-
-          src = pkgs.fetchurl {
-            url =
-              "https://github.com/snatella/wine-runner-sc/releases/download/wine-v${version}/wine-runner-${version}-gold-fsync.tgz";
-            sha256 = "03v0qrhjc0qv3rmx3zb82d3pwqp3ys0r3dhqdplls2nv69ik5b9l";
-          };
-
-          phases = [ "unpackPhase" "installPhase" ];
-          unpackCmd = "tar -xzf $src";
-          sourceRoot = ".";
-          installPhase = "cp -r . $out";
-        };
-      source = pkgs.callPackage package { };
-    in {
-      recursive = false;
-      inherit source;
-      target = "lutris/runners/wine/wine-runner-sc";
-    };
   };
 }
