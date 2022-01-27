2cc0afdfb087e1bff77fa75bee9af7f8926f70cd
Felix Tenley <dev@felschr.com>
2022-01-27 15:06:12 +0100

    feat: update libvirt config

--- virtualisation/virt-manager.nix
+++
@@ -1,8 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-  boot.kernelModules = [ "kvm-amd" "kvm-intel" ];
-  virtualisation.libvirtd.enable = true;
-
-  environment.systemPackages = with pkgs; [ virt-manager ];
-}
