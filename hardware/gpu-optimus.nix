1aae429e500cb6d1a323df71c39472edeb7a1f8d
Felix Tenley <dev@felschr.com>
2022-02-08 00:11:35 +0100

    chore: remove unused gpu-nvidia & gpu-optimus configs

--- hardware/gpu-optimus.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-  # Graphics drivers
-  services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia.optimus_prime.enable = true;
-  hardware.nvidia.optimus_prime.nvidiaBusId = "PCI:2:0:0";
-  hardware.nvidia.optimus_prime.intelBusId = "PCI:0:2:0";
-}
