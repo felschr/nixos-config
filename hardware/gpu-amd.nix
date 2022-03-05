f0797e04860fef0ccc2197c2576b72539ef10eb8
Felix Tenley <dev@felschr.com>
2022-03-05 11:41:21 +0100

    fix: add 32-bit amdvlk as well

--- hardware/gpu-amd.nix
+++ hardware/gpu-amd.nix
@@ -10,15 +10,17 @@
     driSupport32Bit = true;
     extraPackages = with pkgs; [
       amdvlk
-      
+
       vaapiIntel
       vaapiVdpau
       libvdpau-va-gl
-      
+
       rocm-opencl-icd
       rocm-opencl-runtime
     ];
     extraPackages32 = with pkgs.driversi686Linux; [
+      amdvlk
+
       vaapiIntel
       libvdpau-va-gl
       vaapiVdpau
