c7a13f60a2a0866990dbd7c6ba9900f1a96ebd25
Felix Schröter <dev@felschr.com>
2022-05-03 21:18:16 +0200

    fix(neovim): update which-key triggers blacklist

--- home/editors/neovim/which-key.lua
+++ home/editors/neovim/which-key.lua
@@ -1,6 +1,7 @@
 require("which-key").setup {
   triggers_blacklist = {
-    i = { "j", "k", "<c-h>", "<c-j>", "<c-k>", "<c-l>" },
-    v = { "j", "k", "<c-h>", "<c-j>", "<c-k>", "<c-l>" },
+    n = { "<C-h>", "<C-j>", "<C-k>", "<C-l>" },
+    i = { "j", "k" },
+    v = { "j", "k" },
   },
 }
