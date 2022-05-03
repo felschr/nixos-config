832631be55dc4e667ea5dd4ae38f45d4c3eb2259
Felix Schröter <dev@felschr.com>
2022-05-03 20:42:55 +0200

    feat(neovim): add nvim-cmp up/down mappings

--- home/editors/neovim/completion.lua
+++ home/editors/neovim/completion.lua
@@ -16,6 +16,8 @@ cmp.setup({
     end,
   },
   mapping = {
+    ["<Up>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+    ["<Down>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
     ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
     ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
     ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
