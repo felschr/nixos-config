b2a424a021b0ef907d78679b09d664d6637dbfe0
Felix Schröter <dev@felschr.com>
2022-05-03 21:17:43 +0200

    feat(neovim): disable lualine for nvim-tree-lua window

--- home/editors/neovim/lualine.lua
+++ home/editors/neovim/lualine.lua
@@ -1,5 +1,6 @@
 require("lualine").setup {
   options = {
+    disabled_filetypes = { "NVimTree" },
     component_separators = { left = "", right = ""},
     section_separators = { left = "", right = ""},
   },
