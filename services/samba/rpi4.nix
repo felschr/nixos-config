7dc601cf0407ff0b4f41e8b314bc2757bd089ede
Felix Schröter <dev@felschr.com>
2022-05-15 21:21:18 +0200

    feat(home-server): add back media mount

--- services/samba/rpi4.nix
+++ services/samba/rpi4.nix
@@ -4,6 +4,15 @@
   # Use `smbpasswd -a <user>` to set passwords
   # age.secrets.samba.file = ../../secrets/samba.age;
 
+  boot.initrd.luks.devices."enc-media".device =
+    "/dev/disk/by-uuid/47158a41-995a-45d5-b7e1-1dc6e1868be7";
+
+  fileSystems."/media" = {
+    device = "/dev/disk/by-uuid/2441d724-7f8e-4bbb-a50f-9074f3d0d3f1";
+    fsType = "btrfs";
+    options = [ "subvol=@" "compress-force=zstd" "noatime" ];
+  };
+
   services.samba = {
     enable = true;
     openFirewall = true;
