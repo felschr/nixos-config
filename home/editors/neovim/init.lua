fa1dd79cfd95048c42183912c505118d40a2e278
Felix Schröter <dev@felschr.com>
2022-05-27 03:00:27 +0200

    chore(neovim): remove netrw config
    Since I'm only using nvim-tree-lua.

--- home/editors/neovim/init.lua
+++ home/editors/neovim/init.lua
@@ -42,12 +42,6 @@ wk.register({
   ["<C-p>"] = { require("nvim-tree").toggle, "Toggle NVimTree" },
 }, { mode = "n" })
 
-vim.g.netrw_banner = 0
-vim.g.netrw_liststyle = 3
-vim.g.netrw_browse_split = 4
-vim.g.netrw_altv = 1
-vim.g.netrw_winsize = 20
-
 require("toggleterm").setup {
   open_mapping = "<A-t>",
   size = 16,
