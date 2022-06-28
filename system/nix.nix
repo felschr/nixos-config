c739b83a8662881e49ded6acb8e045f5015cf2fe
Felix Schröter <dev@felschr.com>
2022-06-28 02:23:51 +0200

    feat(nix): add own cachix cache

--- system/nix.nix
+++ system/nix.nix
@@ -12,10 +12,15 @@ in {
 
   nix.settings = {
     auto-optimise-store = true;
-    substituters = [ "https://hydra.iohk.io" "https://shajra.cachix.org" ];
+    substituters = [
+      "https://hydra.iohk.io"
+      "https://shajra.cachix.org"
+      "https://felschr.cachix.org"
+    ];
     trusted-public-keys = [
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       "shajra.cachix.org-1:V0x7Wjgd/mHGk2KQwzXv8iydfIgLupbnZKLSQt5hh9o="
+      "felschr.cachix.org-1:raomy5XA2tsVkBoG6wo70ARIn+V24IXhWaSe3QZo12A="
     ];
   };
 
