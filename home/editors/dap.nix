a8adc9a3cac10221c70bff1aff4bbaa1c1bdcea4
Felix Schröter <dev@felschr.com>
2022-07-10 13:19:14 +0200

    feat(editors): add netcoredbg

--- home/editors/dap.nix
+++ home/editors/dap.nix
@@ -1,10 +1,9 @@
 { config, pkgs, ... }:
 
 {
-  home.packages = with pkgs;
-    [
-      # netcoredbg # TODO https://github.com/NixOS/nixpkgs/pull/103940
-      # vscode-firefox-debug # TODO not packaged
-      haskellPackages.haskell-dap
-    ];
+  home.packages = with pkgs; [
+    netcoredbg
+    # vscode-firefox-debug # TODO not packaged
+    haskellPackages.haskell-dap
+  ];
 }
