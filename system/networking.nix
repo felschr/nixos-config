ad8a751b0b9fa3e53ea12ec0e336b9cba7347342
Felix Schröter <dev@felschr.com>
2022-07-13 14:35:32 +0200

    fix(networking): make dnscrypt-proxy2 actually listen on IPv6

--- system/networking.nix
+++ system/networking.nix
@@ -15,6 +15,8 @@
   services.dnscrypt-proxy2 = {
     enable = true;
     settings = {
+      listen_addresses = [ "127.0.0.1:53" "[::1]:53" ];
+
       ipv6_servers = true;
       require_dnssec = true;
 
