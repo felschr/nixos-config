bf307d0979d3346cbebbc0dcdb7a48e2eda30f4f
Felix Schröter <dev@felschr.com>
2022-08-25 22:24:05 +0200

    feat(gaming): add system-wide steam config

--- system/gaming.nix
+++ system/gaming.nix
@@ -3,6 +3,10 @@
 {
   programs.gamemode.enable = true;
 
+  programs.steam.enable = true;
+  programs.steam.remotePlay.openFirewall = true;
+  programs.steam.dedicatedServer.openFirewall = true;
+
   # fix for Star Citizen
   boot.kernel.sysctl."vm.max_map_count" = 16777216;
 }
