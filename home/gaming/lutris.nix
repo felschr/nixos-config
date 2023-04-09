04f0ba7945a913602236240b09164c22ad5392e7
Felix Schröter <dev@felschr.com>
2023-04-09 21:38:03 +0200

    feat(lutris): remove wine-ge

--- home/gaming/lutris.nix
+++ home/gaming/lutris.nix
@@ -3,19 +3,4 @@
 {
   # TODO move into heroic.nix or rename lutris.nix
   home.packages = with pkgs; [ lutris heroic ];
-
-  xdg.dataFile = {
-    wine-ge = let
-      version = "GE-Proton7-35";
-      name = "wine-lutris-${version}-x86_64";
-    in {
-      recursive = false;
-      source = builtins.fetchTarball {
-        url =
-          "https://github.com/GloriousEggroll/wine-ge-custom/releases/download/${version}/${name}.tar.xz";
-        sha256 = "17adirraf6m9kiaa9bkc991h1m5r5g8rbjhgv3ccrvd0v1339zn7";
-      };
-      target = "lutris/runners/wine/${name}";
-    };
-  };
 }
