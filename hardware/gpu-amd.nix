c7822cdf1bf6fcc7ea4a81232f84e2c5880e4a04
Felix Schröter <dev@felschr.com>
2023-04-09 21:48:41 +0200

    feat(hardware): adjust AMD GPU config

--- hardware/gpu-amd.nix
+++ hardware/gpu-amd.nix
@@ -3,29 +3,16 @@
 {
   boot.initrd.kernelModules = [ "amdgpu" ];
 
-  services.xserver.videoDrivers = [ "amdgpu" ];
-
   hardware.opengl = {
     driSupport = true;
     driSupport32Bit = true;
-    extraPackages = with pkgs; [
-      amdvlk
-
-      vaapiIntel
-      vaapiVdpau
-      libvdpau-va-gl
-
-      rocm-opencl-icd
-      rocm-opencl-runtime
-    ];
+    extraPackages = with pkgs; [ vaapiIntel libvdpau-va-gl vaapiVdpau ];
     extraPackages32 = with pkgs.driversi686Linux; [
-      amdvlk
-
       vaapiIntel
       libvdpau-va-gl
       vaapiVdpau
     ];
   };
 
-  environment.variables.AMD_VULKAN_ICD = "RADV";
+  environment.systemPackages = with pkgs; [ glxinfo vulkan-tools ];
 }
