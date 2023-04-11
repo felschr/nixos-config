b8b6127367485dd2e81be2568b5da24ac492c019
Felix Schröter <dev@felschr.com>
2023-04-11 18:00:32 +0200

    feat(system): reset swappiness to default again

--- system/zram.nix
+++ system/zram.nix
@@ -4,5 +4,4 @@ with lib; {
   zramSwap.enable = mkDefault true;
   zramSwap.memoryPercent = mkDefault 100;
   zramSwap.memoryMax = mkDefault (16 * 1024 * 1024 * 1024);
-  boot.kernel.sysctl."vm.swappiness" = mkDefault 100;
 }
