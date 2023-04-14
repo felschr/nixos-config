8506a9b29b8aa6263d753f13c64775ac5ebaa137
Felix Schröter <dev@felschr.com>
2023-04-15 00:46:02 +0200

    feat: use Wayland for Chromium & Electron

--- desktop/wayland.nix
+++ desktop/wayland.nix
@@ -2,4 +2,6 @@
 
 {
   environment.systemPackages = with pkgs; [ wl-clipboard ];
+
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
--- home/signal.nix
+++ home/signal.nix
@@ -1,20 +1,8 @@
 { config, pkgs, ... }:
 
-with pkgs;
-let
-  signal-desktop =
-    runCommand "signal-desktop" { buildInputs = [ makeWrapper ]; } ''
-      mkdir $out
-      ln -s ${pkgs.signal-desktop}/* $out
-      rm $out/bin
-      makeWrapper ${pkgs.signal-desktop}/bin/signal-desktop $out/bin/signal-desktop \
-        --add-flags "--use-tray-icon"
-    '';
-in {
-  home.packages = [ signal-desktop ];
+{
+  home.packages = with pkgs; [ signal-desktop ];
 
-  # TODO switch to overwritten `signal-desktop` when
-  # desktop file is updated with correct exec path
   xdg.configFile."autostart/signal-desktop.desktop".text =
     builtins.replaceStrings [ "bin/signal-desktop" ]
     [ "bin/signal-desktop --start-in-tray" ] (builtins.readFile
