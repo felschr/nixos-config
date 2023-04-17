bc2c479485b07509276a68ee72cd029b94a78846
Felix Schröter <dev@felschr.com>
2023-04-17 12:42:15 +0200

    feat(hardware): update hardened config

--- system/hardened.nix
+++ system/hardened.nix
@@ -3,8 +3,13 @@
 with lib; {
   imports = [ "${modulesPath}/profiles/hardened.nix" ];
 
+  # Xbox Controller not working via Bluetooth if enabled
+  security.lockKernelModules = mkOverride 0 false;
+
   boot.loader.systemd-boot.editor = mkDefault false;
 
   # scudo causes Firefox & Tor Browser segfaults
   environment.memoryAllocator.provider = "libc";
+
+  security.allowSimultaneousMultithreading = mkOverride 0 true;
 }
