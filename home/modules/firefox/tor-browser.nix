a015e13171aca5aea139f7aafabd63e51e195373
Felix Schröter <dev@felschr.com>
2023-04-25 18:20:49 +0200

    feat(home): export mkConfig from firefox modules

--- home/modules/firefox/tor-browser.nix
+++ home/modules/firefox/tor-browser.nix
@@ -4,7 +4,6 @@ let common = import ./common.nix { inherit config lib pkgs; };
 in common.mkModule {
   name = "tor-browser";
   displayName = "Tor Browser";
-  # @TODO is this correct?
   dataConfigPath = ".local/share/tor-browser/TorBrowser/Data/Browser";
   defaultPackage = pkgs.tor-browser-bundle-bin;
   defaultPackageName = "pkgs.tor-browser-bundle-bin";
