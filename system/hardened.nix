c79c125e4c73ef3aea4c8da5ea2670cd48920abb
Felix Schröter <dev@felschr.com>
2023-05-31 17:02:01 +0200

    refactor: update hardened.nix

--- system/hardened.nix
+++ system/hardened.nix
@@ -3,13 +3,16 @@
 with lib; {
   imports = [ "${modulesPath}/profiles/hardened.nix" ];
 
+  # @TODO hardened kernel causes Bluetooth issues
+  boot.kernelPackages = mkOverride 900 pkgs.linuxPackages;
+
   # Xbox Controller not working via Bluetooth if enabled
-  security.lockKernelModules = mkOverride 0 false;
+  security.lockKernelModules = mkOverride 900 false;
 
   boot.loader.systemd-boot.editor = mkDefault false;
 
   # scudo causes Firefox & Tor Browser segfaults
-  environment.memoryAllocator.provider = "libc";
+  environment.memoryAllocator.provider = mkOverride 900 "libc";
 
-  security.allowSimultaneousMultithreading = mkOverride 0 true;
+  security.allowSimultaneousMultithreading = mkOverride 900 true;
 }
