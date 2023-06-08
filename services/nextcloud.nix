8474d93a8c8b191c0667122944795b7aebea6e48
Felix Schröter <dev@felschr.com>
2023-06-08 23:08:02 +0200

    fix(nextcloud): use recommended nginx config for collabora/code

--- services/nextcloud.nix
+++ services/nextcloud.nix
@@ -57,9 +57,19 @@ in {
   services.nginx.virtualHosts."office.felschr.com" = {
     forceSSL = true;
     enableACME = true;
-    locations."/" = {
-      proxyPass = "http://localhost:9980";
-      proxyWebsockets = true;
+    locations = {
+      "^~ /browser".proxyPass = "http://127.0.0.1:9980";
+      "^~ /hosting/discovery".proxyPass = "http://127.0.0.1:9980";
+      "^~ /hosting/capabilities".proxyPass = "http://127.0.0.1:9980";
+      "~ ^/cool/(.*)/ws$" = {
+        proxyPass = "http://127.0.0.1:9980";
+        proxyWebsockets = true;
+      };
+      "~ ^/(c|l)ool".proxyPass = "http://127.0.0.1:9980";
+      "^~ /cool/adminws" = {
+        proxyPass = "http://127.0.0.1:9980";
+        proxyWebsockets = true;
+      };
     };
   };
 
