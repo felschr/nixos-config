7a34b3afaa32508795fe1ea61cb8a9ad49e294f2
Felix Schröter <dev@felschr.com>
2023-08-10 11:39:53 +0200

    feat: enable wayland support for Qt apps

--- desktop/wayland.nix
+++ desktop/wayland.nix
@@ -3,5 +3,8 @@
 {
   environment.systemPackages = with pkgs; [ wl-clipboard ];
 
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+    QT_QPA_PLATFORM = "wayland";
+  };
 }
