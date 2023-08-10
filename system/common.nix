59d27f43677e80f9943435ab6949b75a9752eff7
Felix Schröter <dev@felschr.com>
2023-08-10 11:45:14 +0200

    feat: add rage to common system packages

--- system/common.nix
+++ system/common.nix
@@ -3,5 +3,5 @@
 {
   imports = [ ./zram.nix ./i18n.nix ./nix.nix ./networking.nix ./hardened.nix ];
 
-  environment.systemPackages = with pkgs; [ wget curl openssl neovim ];
+  environment.systemPackages = with pkgs; [ wget curl openssl rage neovim ];
 }
