fab479f5b38b15fcbd8925dcafefd8702689f624
Felix Schröter <dev@felschr.com>
2023-11-19 00:56:22 +0100

    feat(editors): remove unused vscode extension

--- home/editors/dap.nix
+++ home/editors/dap.nix
@@ -1,18 +1,9 @@
-{ config, pkgs, lib, ... }:
+{ pkgs, ... }:
 
-let vscodeExtensions = with pkgs; [ vscode-extensions.ms-vscode.cpptools ];
-in {
+{
   home.packages = with pkgs; [
     netcoredbg
     # vscode-firefox-debug # TODO not packaged
     haskellPackages.haskell-dap
   ];
-
-  home.file = builtins.listToAttrs (map (x: {
-    name = ".vscode/extensions/${x.vscodeExtUniqueId}";
-    value = {
-      source = "${x}/share/vscode/extensions/${x.vscodeExtUniqueId}";
-      recursive = true;
-    };
-  }) vscodeExtensions);
 }
