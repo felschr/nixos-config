b7c472da2d4c0f9fab88cede1419d5335bd7e43a
Felix Schröter <dev@felschr.com>
2023-11-19 16:52:22 +0100

    fix(matrix): fix matrix-appservices bridge encryption

--- services/matrix/appservices.nix
+++ services/matrix/appservices.nix
@@ -9,6 +9,9 @@ let
   bridge_encryption = {
     allow = true;
     default = true;
+    require = true;
+    appservice = true;
+    allow_key_sharing = true;
     key_sharing.allow = true;
     delete_keys.delete_outdated_inbound = false;
   };
