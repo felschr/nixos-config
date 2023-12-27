6ff98a46da8d7109ec7b668960899c7abd407078
Felix Schröter <dev@felschr.com>
2023-12-27 04:32:12 +0100

    feat(browsers): add chromium

--- home/browsers/default.nix
+++ home/browsers/default.nix
@@ -1,7 +1,9 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   imports = [ ./mullvad-browser.nix ./tor-browser.nix ./firefox.nix ];
+
+  home.packages = with pkgs; [ chromium ];
 
   home.sessionVariables.BROWSER = "mullvad-browser";
 }
