13cd6b9ae4a8529b694a949519d796c858e301c3
Felix Schröter <dev@felschr.com>
2025-01-19 14:55:14 +0100

    feat(home): simplify element autostart

--- home/element.nix
+++ home/element.nix
@@ -1,23 +1,22 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 let
-  # wrapper is needed because otherwise desktop file can't be accessed
-  element-desktop = pkgs.symlinkJoin {
-    inherit (pkgs.element-desktop) name src meta;
-    paths = [ pkgs.element-desktop ];
-    nativeBuildInputs = with pkgs; [ makeWrapper ];
-    postBuild = ''
-      rm -rf $out/share/applications
-      mkdir $out/share/applications
-      cat "${pkgs.element-desktop}/share/applications/element-desktop.desktop" \
-        >"$out/share/applications/element-desktop.desktop"
-    '';
+  element-desktop_ = pkgs.element-desktop.override {
+    element-web = pkgs.element-web.override {
+      conf = {
+        showLabsSettings = true;
+      };
+    };
   };
+
 in
 {
-  home.packages = [ element-desktop ];
-
-  xdg.configFile."autostart/element-desktop.desktop".text =
-    builtins.replaceStrings [ "Exec=element-desktop" ] [ "Exec=element-desktop --hidden" ]
-      (builtins.readFile "${element-desktop}/share/applications/element-desktop.desktop");
+  home.packages = with pkgs; [
+    element-desktop
+    (makeAutostartItem {
+      name = "element-desktop";
+      package = element-desktop_;
+      prependExtraArgs = [ "--hidden" ];
+    })
+  ];
 }
