728dba2b555dcee9632758c56d4c2216cfb0ac38
Felix Schröter <dev@felschr.com>
2025-01-19 14:55:36 +0100

    feat(home): simplify Signal autostart

--- home/signal.nix
+++ home/signal.nix
@@ -1,9 +1,12 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
-  home.packages = with pkgs; [ signal-desktop ];
-
-  xdg.configFile."autostart/signal-desktop.desktop".text =
-    builtins.replaceStrings [ "bin/signal-desktop" ] [ "bin/signal-desktop --start-in-tray" ]
-      (builtins.readFile "${pkgs.signal-desktop}/share/applications/signal-desktop.desktop");
+  home.packages = with pkgs; [
+    signal-desktop
+    (makeAutostartItem {
+      name = "signal-desktop";
+      package = signal-desktop;
+      prependExtraArgs = [ "--start-in-tray" ];
+    })
+  ];
 }
