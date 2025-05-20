d1a110c12d7e104e9bcbac6886359594e94710f9
Felix Schröter <dev@felschr.com>
2025-05-20 21:13:42 +0200

    fix(home-server): disable wait-online service for initrd

--- hosts/home-server/default.nix
+++ hosts/home-server/default.nix
@@ -145,6 +145,7 @@ in
       authorizedKeys = config.users.users.felschr.openssh.authorizedKeys.keys;
     };
   };
+  boot.initrd.systemd.network.wait-online.enable = false;
   # allow automated decryption
   # `echo -n '<LUKS passphrase here>' | clevis encrypt tang '{"url": "http://doctr:9090"}' > home-server-enc.jwe`
   boot.initrd.clevis.enable = true;
