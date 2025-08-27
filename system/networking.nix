4a06b455b97c196ddaffa7d0688e27b024f57a1a
Felix Schröter <dev@felschr.com>
2025-08-27 16:14:11 +0200

    feat(networking): add wifi-auto-toggle dispatcher script

--- system/networking.nix
+++ system/networking.nix
@@ -122,6 +122,27 @@ in
 
   networking.networkmanager.dispatcherScripts = [
     {
+      source = pkgs.writeShellScript "wifi-auto-toggle" ''
+        LOG_PREFIX="WiFi Auto-Toggle"
+
+        if [[ "$1" =~ ^(enp|eth) ]]; then
+          case "$2" in
+            up)
+              echo "$LOG_PREFIX ethernet up"
+              ${pkgs.networkmanager}/bin/nmcli radio wifi off
+              ;;
+            down)
+              echo "$LOG_PREFIX ethernet down"
+              ${pkgs.networkmanager}/bin/nmcli radio wifi on
+              ;;
+          esac
+        elif [[ "$(${pkgs.networkmanager}/bin/nmcli -g GENERAL.STATE device show $ETHERNET_INTERFACE)" = "20 (unavailable)" ]]; then
+          echo "$LOG_PREFIX failsafe"
+          ${pkgs.networkmanager}/bin/nmcli radio wifi on
+        fi
+      '';
+    }
+    {
       source = pkgs.writeShellScript "connect-ice" ''
         ACTION="$2"
         if [[ "$ACTION" == "up" ]]; then
