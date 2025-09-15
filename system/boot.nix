29eceee7eb25c6cc41326977b95c2e2aa7168bf7
Felix Schröter <dev@felschr.com>
2025-09-15 20:14:55 +0200

    feat(system): set systemd-boot configuration limit to 10
    In addition to Nix garbage collection based on age, this will keep the
    boot partition from running full with too many generations.

--- system/boot.nix
+++ system/boot.nix
@@ -4,6 +4,7 @@
   boot.supportedFilesystems = lib.mkDefault [ "btrfs" ];
   boot.kernelPackages = lib.mkOverride 800 pkgs.linuxPackages_latest;
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 10;
   boot.loader.efi.canTouchEfiVariables = true;
 
   boot.initrd.systemd.enable = true;
